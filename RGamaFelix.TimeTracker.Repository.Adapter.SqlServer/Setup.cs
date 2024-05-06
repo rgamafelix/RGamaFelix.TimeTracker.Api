704c3b962f079172ee822e941f4037fa0fdd0910
Rodrigo Gama Felix <gamafelix@gmail.com>
2024-05-06 09:59:48 -0300

    Adding support for ms-sqlserver

---
+++ RGamaFelix.TimeTracker.Repository.Adapter.SqlServer/Setup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RGamaFelix.TimeTracker.Repository.Adapter.SqlServer;
+
+public static class Setup
+{
+    public static IServiceCollection UseSqlServer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TimeTrackerDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("TimeTrackerDbConnectionString"),
+                    options => { options.MigrationsAssembly("RGamaFelix.TimeTracker.Repository.Adapter.SqlServer"); })
+#if DEBUG
+                .EnableSensitiveDataLogging().EnableDetailedErrors()
+#endif
+                ;
+        });
+        return services;
+    }
+}
