bbf3b0fc8bbbfc7d94ade2fba5aba06a5cc0bc88
Rodrigo Gama Felix <gamafelix@gmail.com>
2024-05-06 10:02:41 -0300

    Removing AuthenticationResolver logic and classes -- Behavior has been trasferred to Mediatr pipeline

--- RGamaFelix.TimeTracker.ApplicationService/Contracts/IAuthenticationResolver.cs
+++
@@ -1,9 +0,0 @@
-using System.Security.Claims;
-
-namespace RGamaFelix.TimeTracker.ApplicationService.Contracts;
-
-public interface IAuthenticationResolver
-{
-    bool Resolve(bool requireAuthentication, IEnumerable<string>? permittedRoles = null,
-        IEnumerable<Claim>? requiredClaims = null);
-}
